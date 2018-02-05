bf766a11a9daf3ca7b7d1db01119d06901fd7247
nikitha <nikitha@qburst.com>
2018-02-05 17:27:31 +0530

    Initial commit

---
+++ Scripts.js
@@ -0,0 +1,14 @@
+$(function(){
+  $('#city_lover').click(function(e){
+    e.preventDefault();
+    $('#headerImage').css('background','url(/images/Regular_size/Home_hero1.jpg)','transition','background-image 2s ease-out;');
+  });
+  $('#village_dweller').click(function(e){
+       e.preventDefault();
+      $('#headerImage').css('background-image','url(/images/Regular_size/Home_hero2.jpg)','transition','background-image 2s ease-out;');
+  });
+  $('#nature_seeker').click(function(e){
+    e.preventDefault();
+    $('#headerImage').css('background','url(/images/Regular_size/Home_hero3.jpg)','transition','background-image 2s ease-out;');
+});
+});
